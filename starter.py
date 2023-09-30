4bd887502a63ee44763acbbe86c70766ce720ab4
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-10-01 01:06:56 +0900

    손을 직선으로 따라가도록, 후에 랜덤 손위치

--- starter.py
+++ starter.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import random
+import math
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
@@ -8,40 +9,37 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
-def handle_events():
+def random_hand():
     global x, y
+    x = random.randint(0, TUK_WIDTH)
+    y = random.randint(0, TUK_HEIGHT)
 
-    x = random.randint(0,TUK_WIDTH)
-    y = random.randint(0,TUK_HEIGHT)
-
-    pass
-
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2 #가야할 위치
-x1,y1 = 0,0
-x2,y2 = 0,0
-
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2  # 가야할 위치
+character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2  # 캐릭터의 현재 위치
+random_hand()
 
 frame = 0
 hide_cursor()
 
-while True: #랜덤하게 손 위치 정하기
+while True:  # 랜덤하게 손 위치 정하기
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    hand_arrow.clip_draw(0, 0, 50, 50, x, y) #가야할 위치 정하고
-    delay(0.5) #시간 딜레이
+    hand_arrow.clip_draw(0, 0, 50, 50, x, y)  # 가야할 위치 정하고
 
+    dx = x - character_x
+    dy = y - character_y
+    distance = math.sqrt(dx ** 2 + dy ** 2)
 
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x2, y2)
+    if distance > 0:
+        speed = 0.5
+        character_x += (dx / distance) * speed
+        character_y += (dy / distance) * speed
+
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
     update_canvas()
     frame = (frame + 1) % 8
 
-
-    delay(0.03)
-    handle_events()
-
+    if distance <= speed:
+        random_hand()
 
 close_canvas()
-
-
-
-
