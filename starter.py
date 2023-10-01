6189e7d9a95cc12debfe35e1a57761f81e9dc77b
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-10-01 23:11:47 +0900

    캐릭터 좌우방향 추가 -완성

--- starter.py
+++ starter.py
@@ -17,9 +17,20 @@ def random_hand():
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2  # 가야할 위치
 character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2  # 캐릭터의 현재 위치
 random_hand()
+character_scale_x = 1
 
 frame = 0
 hide_cursor()
+
+def set_character_direction(dx):
+    # 캐릭터의 이동 방향을 설정하는 함수
+    global character_scale_x
+
+    if dx > 0:
+        character_scale_x = 1  # 오른쪽으로 이동하면 원래 방향대로
+    else:
+        character_scale_x = -1  # 왼쪽으로 이동하면 이미지를 뒤집음
+
 
 while True:  # 랜덤하게 손 위치 정하기
     clear_canvas()
@@ -32,10 +43,15 @@ while True:  # 랜덤하게 손 위치 정하기
 
     if distance > 0:
         speed = 0.5
+        set_character_direction(dx)  # 캐릭터 방향 설정
         character_x += (dx / distance) * speed
         character_y += (dy / distance) * speed
 
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    if character_scale_x == 1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    elif character_scale_x == -1:
+        character.clip_draw(frame*100,100* 0, 100,100,character_x,character_y)
+
     update_canvas()
     frame = (frame + 1) % 8
 
